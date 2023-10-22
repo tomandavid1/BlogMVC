9ea0577cbaef5751ff118164b615839d4a745732
Pruone <tomandavid1@seznam.cz>
2023-10-22 19:52:55 +0200

    adding-partial-views

---
+++ Blog.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+	public class AdminBlogPostsController : Controller
+	{
+
+		[HttpGet]
+		public IActionResult Add()
+		{
+			return View();
+		}
+	}
+}
