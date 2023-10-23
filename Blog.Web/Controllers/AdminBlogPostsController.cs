9012d73a389c0a68bf7dcf79bcadf008bee41368
Pruone <tomandavid1@seznam.cz>
2023-10-23 10:25:14 +0200

    add-blog-posts-page

--- Blog.Web/Controllers/AdminBlogPostsController.cs
+++ Blog.Web/Controllers/AdminBlogPostsController.cs
@@ -1,14 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Blog.Web.Controllers
 {
 	public class AdminBlogPostsController : Controller
 	{
+		private readonly ITagRepository tagRepository;
+
+		public AdminBlogPostsController(ITagRepository tagRepository)
+		{
+			this.tagRepository = tagRepository;
+		}
 
 		[HttpGet]
-		public IActionResult Add()
+		public async Task<IActionResult> AddAsync()
 		{
-			return View();
+			// Get Tags from repository
+
+			var tags = await tagRepository.GetAllAsync();
+
+			var model = new AddBlogPostRequest
+			{
+				Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() })
+			};
+
+			return View(model);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddAsync(AddBlogPostRequest addBlogPostRequest)
+		{
+			return RedirectToAction("Add");
 		}
 	}
 }
